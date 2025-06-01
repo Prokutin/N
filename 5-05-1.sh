ceb7da89de5da739405b2a07b775154a03c2c733
Prokutin <q152827060@yandex.ru>
2025-06-01 23:31:42 +0300

    v2

--- 5-05-1.sh
+++ 5-05-1.sh
@@ -1,35 +1,21 @@
 #!/bin/bash
 PREFIX="${1:-NOT_SET}"
 INTERFACE="$2"
-SUBNET_RANGE="${3:-0..255}"
-HOST_RANGE="${4:-1..254}"
+SUBNET="${3:-0..255}"
+HOST="${4:-1..254}"
+oct1="${PREFIX%%.*}"
+oct2="${PREFIX#*.}"
 
 if [[ $EUID -ne 0 ]]; then
     echo "ERROR: This script must be run with elevated privileges (sudo)" >&2
     exit 1
-fi
+fi 
 
 [[ "$PREFIX" = "NOT_SET" ]] && { echo "\$PREFIX must be passed as first positional argument"; exit 1; }
 if [[ -z "$INTERFACE" ]]; then
     echo "\$INTERFACE must be passed as second positional argument"
     exit 1
 fi
-
-if [[ ! "$PREFIX" =~ ^[0-9]+\.[0-9]+$ ]]; then
-    echo "ERROR: \$PREFIX must be in format N.N ( 192.168)" >&2
-    exit 1
-fi
-
-if [[ ! "$SUBNET_RANGE" =~ ^([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))?$ ]]; then
-    echo "ERROR: \$SUBNET_RANGE must be a number (0-255) or range (0..255)" >&2
-    exit 1
-fi
-
-if [[ ! "$HOST_RANGE" =~ ^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-4])(\.\.([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-4]))?$ ]]; then
-    echo "ERROR: \$HOST_RANGE must be a number (1-254) or range (1..254)" >&2
-    exit 1
-fi
-
 
 check_range() {
     local range=$1
@@ -40,10 +26,36 @@ check_range() {
     fi
 }
 
-for SUBNET in $(check_range "$SUBNET_RANGE")
+check_parameters() {
+    local value="$1"
+    local min="$2"
+    local max="$3"
+
+	if [[ "$value" == *".."* ]]; then
+        return 0
+    fi
+
+    if [[ ! "$value" =~ ^[0-9]+$ ]]; then
+        return 1
+    fi
+
+    if [[ "$value" -ge "$min" && "$value" -le "$max" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+check_parameters "$oct1" 0 254 || { echo "ERROR: " ${PREFIX} "must be a ip (0.0 - 254.254)"  ; exit 1; }
+check_parameters "$oct2" 0 254 || { echo "ERROR: " ${PREFIX} "must be a ip (0.0 - 254.254)"  ; exit 1; }
+check_parameters "$SUBNET" 0 255 || { echo "ERROR: " ${SUBNET} "must be a number (0-255)"  ; exit 1; }
+check_parameters "$HOST" 1 254 || { echo "ERROR: " ${HOST} "must be a number (1-254)"  ; exit 1; }
+
+
+for SUBNET in $(check_range "$SUBNET")
 
 do
-	for HOST in $(check_range "$HOST_RANGE")
+	for HOST in $(check_range "$HOST")
 	do
 		echo "[*] IP : ${PREFIX}.${SUBNET}.${HOST}"
 		arping -c 3 -i "$INTERFACE" "${PREFIX}.${SUBNET}.${HOST}" 2> /dev/null
